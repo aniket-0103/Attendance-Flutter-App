2840d6c20da9d6fa91e72fb8a1b1de1c0acfc420
aniket-0103 <75750757+aniket-0103@users.noreply.github.com>
2021-05-04 19:42:06 +0530

    Add files via upload

--- lib/Screens/database.dart
+++ lib/Screens/database.dart
@@ -1,7 +1,8 @@
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Database extends StatefulWidget {
   @override
@@ -11,9 +12,13 @@ class Database extends StatefulWidget {
 class _DatabaseState extends State<Database> {
   static String full_name;
   var lecture;
+  var uid;
   var age;
   var time;
+  var url= "http://127.0.2.2:5000/";
+  PickedFile _imageFile;
   User loggedInUser;
+  final picker = ImagePicker();
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
   FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -21,6 +26,27 @@ class _DatabaseState extends State<Database> {
   void initState() {
     super.initState();
     getCurrentUser();
+  }
+
+  Future<bool> uploadImage(url) async {
+    var filepath;
+    try {
+      final pickedImage = await picker.getImage(source: ImageSource.camera);
+      if(pickedImage != null)
+        filepath = pickedImage.path;
+
+      setState(() {
+        _imageFile = pickedImage;
+      });
+    } catch (e) {
+      print("Image picker error " + e);
+    }
+
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+    request.files.add(await http.MultipartFile.fromPath('image', filepath));
+    var res = await request.send();
+    print(res);
+    return false;
   }
 
   void getCurrentUser() async {
@@ -121,38 +147,37 @@ class _DatabaseState extends State<Database> {
                   ),
                 ),
               ),
-              // SizedBox(
-              //   height: 8.0,
-              // ),
-              // TextField(
-              //   style:  TextStyle(color: Colors.black) ,
-              //   keyboardType: TextInputType.number,
-              //   onChanged: (value) {
-              //     age = value;
-              //   },
-              //   decoration: InputDecoration(
-              //     hintText: 'Enter your password.',
-              //     labelText: 'Age',
-              //     labelStyle: TextStyle(
-              //         color: Colors.black45
-              //     ),
-              //     contentPadding:
-              //     EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              //     border: OutlineInputBorder(
-              //       borderRadius: BorderRadius.all(Radius.circular(32.0)),
-              //     ),
-              //     enabledBorder: OutlineInputBorder(
-              //       borderSide:
-              //       BorderSide(color: Colors.lightBlueAccent, width: 1.0),
-              //       borderRadius: BorderRadius.all(Radius.circular(32.0)),
-              //     ),
-              //     focusedBorder: OutlineInputBorder(
-              //       borderSide:
-              //       BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-              //       borderRadius: BorderRadius.all(Radius.circular(32.0)),
-              //     ),
-              //   ),
-              // ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                style:  TextStyle(color: Colors.black) ,
+                keyboardType: TextInputType.text,
+                onChanged: (value) {
+                  uid = value;
+                },
+                decoration: InputDecoration(
+                  labelText: 'User ID',
+                  labelStyle: TextStyle(
+                      color: Colors.black45
+                  ),
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
               SizedBox(
                 height: 24.0,
               ),
@@ -164,11 +189,15 @@ class _DatabaseState extends State<Database> {
                   elevation: 5.0,
                   child: MaterialButton(
                     onPressed: (){
-                      time = new DateTime.now();
-                      final user = _firestore.collection(full_name).add({
-                        'Date': time,
-                        'Lecture': lecture
-                      });
+                      var recognition_result = uploadImage(url);
+                      print(recognition_result);
+                      if(recognition_result != false)
+                      {
+                        time = new DateTime.now();
+                        final user = _firestore.collection(full_name).add({
+                          'Date': time,
+                          'Lecture': lecture
+                        });}
                       Scaffold.of(context).showSnackBar(SnackBar(
                         content: Text("Attendance recorded"),
                       ));
@@ -180,35 +209,33 @@ class _DatabaseState extends State<Database> {
                       style: TextStyle(
                         color: Colors.white
                       ),
-
-
                     ),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                child: Material(
-                  elevation: 5.0,
-                  color: Colors.lightBlueAccent,
-                  borderRadius: BorderRadius.circular(30.0),
-                  child: MaterialButton(
-                    onPressed: () {
-                      //Go to login screen.
-                      Navigator.pushNamed(context, '/face_recognition');
-                      // time = new DateTime.now();
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Text(
-                      'Capture face',
-                    ),
-                  ),
-                ),
-              ),
+              // SizedBox(
+              //   height: 10.0,
+              // ),
+              // Padding(
+              //   padding: EdgeInsets.symmetric(vertical: 16.0),
+              //   child: Material(
+              //     elevation: 5.0,
+              //     color: Colors.lightBlueAccent,
+              //     borderRadius: BorderRadius.circular(30.0),
+              //     child: MaterialButton(
+              //       onPressed: () {
+              //         //Go to login screen.
+              //         Navigator.pushNamed(context, '/face_recognition');
+              //         // time = new DateTime.now();
+              //       },
+              //       minWidth: 200.0,
+              //       height: 42.0,
+              //       child: Text(
+              //         'Capture face',
+              //       ),
+              //     ),
+              //   ),
+              // ),
             ],
           ),
         ),
