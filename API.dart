16bd3d359a39f5b9cc0269bfa1d4d6499cadc783
aniket-0103 <75750757+aniket-0103@users.noreply.github.com>
2021-05-12 14:13:24 +0530

    Delete API.dart

--- API.dart
+++
@@ -1,8 +0,0 @@
-import 'package:http/http.dart' as http;
-
-Future Getdata(url) async {
-  print("hi");
-  http.Response Response = await http.get(url);
-  print("returning");
-  return Response.body;
-}
